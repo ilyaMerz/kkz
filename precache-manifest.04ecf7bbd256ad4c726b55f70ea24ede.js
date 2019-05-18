86758f0c61c2f5003284a58d7bce41b517f2368d
Ilya Merzlyakov <ilyamerzlyakov@gmail.com>
2019-05-18 19:11:17 +0300

    Updates

---
+++ precache-manifest.04ecf7bbd256ad4c726b55f70ea24ede.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e249d954044bc70e5611",
+    "url": "/static/js/main.11851790.chunk.js"
+  },
+  {
+    "revision": "a311ff972efdb2a6f647",
+    "url": "/static/js/2.0aa7bf91.chunk.js"
+  },
+  {
+    "revision": "e249d954044bc70e5611",
+    "url": "/static/css/main.9828560f.chunk.css"
+  },
+  {
+    "revision": "fc50424331cf734521430e0a061c4be7",
+    "url": "/index.html"
+  }
+];
